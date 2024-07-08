91af8aa779800e22a2698cc1619bac69faf382aa
Delfeil Casanova <delfeilcasanova@gmail.com>
2024-07-08 11:54:37 +0200

    [CHANGE] Add a description to skills

--- src/app/interfaces/skill.ts
+++ src/app/interfaces/skill.ts
@@ -3,5 +3,6 @@ import { SkillsCategory } from "../enums/skills-category";
 export interface Skill {
     category: SkillsCategory,
     name: string,
-    link: string
+    link: string,
+    description: string
 }
